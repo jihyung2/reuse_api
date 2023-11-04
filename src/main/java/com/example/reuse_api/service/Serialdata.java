064046fe3502976d737d8c38c74650ed00ab2368
HG-Shin <sinhuigwan7@naver.com>
2023-11-04 20:01:22 +0900

    Serialdata 예외처리 추가

--- src/main/java/com/example/reuse_api/service/Serialdata.java
+++ src/main/java/com/example/reuse_api/service/Serialdata.java
@@ -1,84 +1,100 @@
 package com.example.reuse_api.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fazecast.jSerialComm.SerialPort;
-import jakarta.annotation.PostConstruct;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.http.*;
-import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+
 import java.util.HashMap;
 import java.util.Map;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 
 @Service
 public class Serialdata {
+    private static final String SERIAL_PORT = "/dev/cu.usbserial-A10NLRGZ";
+    private static final String REST_API_SERVER = "http://127.0.0.1:8080/sensor";
+    private static final int MAX_RETRY_COUNT = 5;
+    private static final int RETRY_DELAY_MS = 2000;
 
     private StringBuilder buffer = new StringBuilder();
 
-
     public void readSerialData() {
-        SerialPort serialPort = SerialPort.getCommPort("/dev/cu.usbserial-A10NLRGZ"); // 시리얼 포트 경로 지정
+        int retryCount = 0;
+        while (true) {
+            try {
+                readFromSerialPort();
+                break;
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println("Failed to connect to the serial port. Retrying...");
+                retryCount++;
+                if (retryCount > MAX_RETRY_COUNT) {
+                    System.out.println("Failed to connect to the serial port after " + MAX_RETRY_COUNT + " attempts. Please check the connection.");
+                    break;
+                }
+                try {
+                    Thread.sleep(RETRY_DELAY_MS);
+                } catch (InterruptedException ie) {
+                    ie.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void readFromSerialPort() throws Exception {
+        SerialPort serialPort = SerialPort.getCommPort(SERIAL_PORT);
         serialPort.openPort();
         serialPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 0, 0);
         System.out.println("Serial Communication Success!");
 
-
-        try {
-            while (true) {
-                if (serialPort.bytesAvailable() > 0) {
-                    byte[] data = new byte[serialPort.bytesAvailable()];
-                    serialPort.readBytes(data, data.length);
-
-                    String receivedData = new String(data, "UTF-8");
-
-                    System.out.println("Received data: " + receivedData);
-
-                    buffer.append(receivedData); //문자열 버퍼(문자열 누적기) 수신 받은 데이터 누적
-                    // 차례차례 처리하는 코드임, 하나의 한 세트($,&) 처리
-                    int startIndex = buffer.indexOf("$");
-                    int endIndex = buffer.indexOf("&");
-                    System.out.println("buffer: " + buffer.toString());
-                    if (startIndex >= 0 && endIndex > startIndex){ //&가 없으면 -1 출력함, end>start 는 $시작해서 &끝나는 메세지만 있을때 처리
-                         // 받은 데이터 처리
-                        String message = buffer.substring(startIndex + 1, endIndex);
-
-                        RestTemplate restTemplate = new RestTemplate();
-
-                        Map<String, String> map = new HashMap<>();
-
-                        map.put("data", message);
-                        map.put("name", "UART data");
-
-                        //map을 json 형태로 보내려면 objectMapper을 해줘야한다.
-                        ObjectMapper objectMapper = new ObjectMapper();
-                        String jsonData = objectMapper.writeValueAsString(map);
-
-                        HttpHeaders headers = new HttpHeaders();
-                        headers.setContentType(MediaType.APPLICATION_JSON);
-                        System.out.println(map);
-
-                        HttpEntity<String> requestEntity = new HttpEntity<>(jsonData, headers);
-                        String restapi_server = "http://127.0.0.1:8080/sensor";
-
-                        ResponseEntity<String> response = restTemplate.postForEntity(restapi_server, requestEntity, String.class);
-
-                        if (response.getStatusCodeValue() == 200){
-                            System.out.println("데이터 전송 성공");
-                        }
-                        else {
-                            System.out.println("데이터 전송 실패");
-                        }
-                        buffer.delete(startIndex, endIndex + 1); //$, & 안에있는 데이터 썼으니 지움
-
-                    }
-                }
+        while (true) {
+            if (serialPort.bytesAvailable() > 0) {
+                processDataFromSerialPort(serialPort);
             }
-        } catch (Exception e) {
+        }
+    }
+
+    private void processDataFromSerialPort(SerialPort serialPort) throws Exception {
+        byte[] data = new byte[serialPort.bytesAvailable()];
+        serialPort.readBytes(data, data.length);
+        String receivedData = new String(data, "UTF-8");
+        System.out.println("Received data: " + receivedData);
+        buffer.append(receivedData);
+        int startIndex = buffer.indexOf("$");
+        int endIndex = buffer.indexOf("&");
+        System.out.println("buffer: " + buffer.toString());
+        if (startIndex >= 0 && endIndex > startIndex) {
+            String message = buffer.substring(startIndex + 1, endIndex);
+            sendToRestApi(message);
+            buffer.delete(startIndex, endIndex + 1);
+        }
+    }
+
+    private void sendToRestApi(String message) {
+        RestTemplate restTemplate = new RestTemplate();
+        Map<String, String> map = new HashMap<>();
+        map.put("data", message);
+        map.put("name", "UART data");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonData = null;
+        try {
+            jsonData = objectMapper.writeValueAsString(map);
+        } catch (JsonProcessingException e) {
             e.printStackTrace();
+        }
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        System.out.println(map);
+        HttpEntity<String> requestEntity = new HttpEntity<>(jsonData, headers);
+        ResponseEntity<String> response = restTemplate.postForEntity(REST_API_SERVER, requestEntity, String.class);
+        if (response.getStatusCodeValue() == 200) {
+            System.out.println("데이터 전송 성공");
+        } else {
+            System.out.println("데이터 전송 실패");
         }
     }
 }
