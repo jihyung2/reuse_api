5fcac9367f46bb0e8d905e97f5f66757c8f34957
HG-Shin <sinhuigwan7@naver.com>
2023-11-06 19:25:52 +0900

    Image Show Test

--- src/main/java/com/example/reuse_api/controller/ImageDataController.java
+++ src/main/java/com/example/reuse_api/controller/ImageDataController.java
@@ -6,24 +6,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
 public class ImageDataController {
 
     @Autowired
-    private ImageService imageDataService;
+    private ImageService imageService;
 
-    @GetMapping(value = "/image/{id}", produces = MediaType.IMAGE_JPEG_VALUE)
-    public void getImage(@PathVariable Long id, HttpServletResponse response) throws IOException {
-        Optional<ImageData> imageDataOptional = imageDataService.getImageData(id);
-        if(imageDataOptional.isPresent()) {
-            ImageData imageData = imageDataOptional.get();
-            response.getOutputStream().write(imageData.getData());
-        }
+    @GetMapping("/image-ids")
+    public @ResponseBody List<Long> getImageIds() {
+        return imageService.getAllImageIds();
     }
 }
